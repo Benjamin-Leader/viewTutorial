80cdb817987c5cbd4c39ee795a9c1327701d327e
=Ben Leader <=>
2014-09-10 21:59:42 -0700

    Finished

---
+++ viewTutorial/BLGrid.h
@@ -0,0 +1,13 @@
+//
+//  BLGrid.h
+//  viewTutorial
+//
+//  Created by Ben Leader on 9/10/14.
+//  Copyright (c) 2014 Benjamin Leader. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLGrid : UIView
+
+@end
---
+++ viewTutorial/BLGrid.m
@@ -0,0 +1,31 @@
+//
+//  BLGrid.m
+//  viewTutorial
+//
+//  Created by Ben Leader on 9/10/14.
+//  Copyright (c) 2014 Benjamin Leader. All rights reserved.
+//
+
+#import "BLGrid.h"
+
+@implementation BLGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
